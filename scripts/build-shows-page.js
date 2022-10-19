9be94ec89051ba9f95b06d1f9cc6a9b7fc906239
Nataliia Sokolova <natalia.sokolova.ca@gmail.com>
2022-10-19 12:00:45 -0700

    added js objects for show

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -1,6 +1,33 @@
 const card = document.createElement("div");
 card.classList = "card-body";
 
+// Dynamically change the styles of an element
+window.addEventListener("resize", function (event) {
+  if (window.innerWidth <= 320) {
+    cardElement.update({ style: { base: { fontSize: "13px" } } });
+  } else {
+    cardElement.update({ style: { base: { fontSize: "16px" } } });
+  }
+});
+
+const data = [
+  {
+    date: "Mon Sept 06 2021",
+    venue: "Ronald Lane",
+    location: "San Francisco, CA",
+  },
+  {
+    date: "Mon Sept 06 2021",
+    venue: "Ronald Lane",
+    location: "San Francisco, CA",
+  },
+  {
+    date: "Mon Sept 06 2021",
+    venue: "Ronald Lane",
+    location: "San Francisco, CA",
+  },
+];
+
 const button = document.querySelector("button");
 button.addEventListener(
   "click",
--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,15 +1,11 @@
-var x = document.createElement("FORM");
-
 let comments = [
   {
     id: 1,
     author: "John Q. Commenter",
-    text: "This is the first comment.",
     timestamp: new Date(),
   },
   {
     id: 2,
-    author: "Jane A. Opinionated",
     text: "This is the second comment.",
     timestamp: new Date(),
   },
@@ -20,8 +16,9 @@ let comments = [
 ];
 
 function displayComment(commentObj) {
-  const commentSection = document.querySelector("main__comments");
+  const commentSection = document.querySelector("section");
   commentSection.classList.add("comments__item");
   const individualComment = document.createElement("div");
   commentSection.appendChild(individualComment);
+  individualComment.innerText = comments[0].text;
 }
